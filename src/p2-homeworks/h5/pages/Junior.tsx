152539a2612486b2c1a49bea76d333643e9ce235
Alex <akiosa88@gmail.com>
2022-07-12 00:06:56 +0400

    HW7

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Junior() {
+    return (
+        <div>
+            {/*<HW7/>*/}
+            HW7 - в ожидании чуда =)
+        </div>
+    )
+}
+
+export default Junior
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW8/>*/}
+            HW7 - ты уже поти маг :)
+        </div>
+    )
+}
+
+export default JuniorPlus
